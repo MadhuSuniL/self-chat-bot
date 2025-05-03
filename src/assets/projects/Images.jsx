44380c583bfa8a42815e96dbe02f3e1ab74f2700
Madhu <114853422+MadhuSuniL@users.noreply.github.com>
2025-05-03 14:20:09 +0530

    Add files via upload

--- src/assets/projects/Images.jsx
+++ src/assets/projects/Images.jsx
@@ -1,19 +1,35 @@
-import POS from './pos.png';
-import POS2 from './pos2.png';
-import CRM from './crm.png';
+import AI_AGENTS from './ai_agents.jpg';
+import GPT_SUITE from './chat_gpt.jpg';
+import CUSTOM_GPT from './custom_gpts.jpg';
+import PERSONA_BUILDER from './persona_builder.jpg';
 import PMS from './pms.png';
-import UX9 from './ux9.png';
-import SIMMI from './simmi.png';
-import AIMOCK from './ai_mock.png';
+import CRM from './crm.png';
+import USER_MANAGEMENT from './user_management.jpg';
+import CC_FRAUD from './credit_card.jpg';
+import DYNAMIC_PRICING from './center_price.jpg';
+import RESUME_PARSER from './resume_parser.jpg';
+import FACE_MASK from './musk_detection.jpg';
+import VISUAL_SEARCH from './visual_search.jpg';
+import SEQ2SEQ_CHATBOT from './chat_bot.jpg'; // You can use another if available
+import TEXT_TO_IMAGE from './text_img.jpg';
+import SPEECH_EMOTION from './sound_rec.jpg';
 
 const Images = {
-    POS,
-    POS2,
-    CRM,
+    AI_AGENTS,
+    GPT_SUITE,
+    CUSTOM_GPT,
+    PERSONA_BUILDER,
     PMS,
-    UX9,
-    SIMMI,
-    AIMOCK
+    CRM,
+    USER_MANAGEMENT,
+    CC_FRAUD,
+    DYNAMIC_PRICING,
+    RESUME_PARSER,
+    FACE_MASK,
+    VISUAL_SEARCH,
+    SEQ2SEQ_CHATBOT,
+    TEXT_TO_IMAGE,
+    SPEECH_EMOTION,
 };
 
 export default Images;
